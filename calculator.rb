b69f2849b6e02f84511ac9514c1a31437172e2af
perohrstrom <per.k.ohrstrom@gmail.com>
2016-07-25 01:16:43 -0400

    Print Calculations Results after exiting

--- calculator.rb
+++ calculator.rb
@@ -12,12 +12,25 @@ if operator == "+"
 
 end
 
-puts "Please enter an equation you'd like to run."
-equation = gets.chomp
-equation = equation.split(' ')
+puts "Please enter an equation you'd like to run. When you're done - just enter 'exit'"
+equation = nil
+calculation_array = []
+count = 0
+equation = gets.chomp.downcase
+until equation == 'exit'
+    equation_query = equation.split(' ')
 
-x = equation[0].to_i
-operator = equation[1]
-z = equation[2].to_i
+          x = equation_query[0].to_i
+          operator = equation_query[1]
+          z = equation_query[2].to_i
+          answer= calc(x,operator,z)
+          puts answer
 
-puts calc(x,operator,z)
+    calc_string = "#{equation} " + " " + "= #{answer}"
+    calculation_array.push calc_string
+  count += 1
+  puts "Keep going. When you are tired - just enter 'exit'"
+  equation = gets.chomp.downcase
+end
+puts "Thanks for using the calculator! There were #{count} calculations performed:"
+puts calculation_array
