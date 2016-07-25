ab5b5788e86fecdbc4371aaac6e9a3019c793c5e
perohrstrom <per.k.ohrstrom@gmail.com>
2016-07-25 00:42:13 -0400

    Debugged Calc Problems

--- calculator.rb
+++ calculator.rb
@@ -1,16 +1,24 @@
-def calc(x,y,z)
-  x.send y,z
+def calc(x,operator,z)
+if operator == "+"
+    x + z
+  elsif operator == "-"
+    x - z
+  elsif operator == "*"
+    x * z
+  elsif operator == "/"
+    x / z
+  else
+  end
+
 end
 
 puts "Please enter a number"
-x = gets.chomp
+x = gets.to_i
+
 puts "Now enter which math operator you'd like to use: +, -, *, or /"
-y = gets.chomp
-  until y == "+" || y == "-" || y == "*" || y_string == "/"
-    puts "Please enter +, -, *, or /"
-  end
+operator = gets.chomp
 
 puts "Now enter another number."
-z = gets.chomp
+z = gets.to_i
 
-puts calc(x,y,z)
+puts calc(x,operator,z)
